92a4b37a799890471b1ceda32ee84059f1a60fa9
flat35hd99 <flat35hd99@gmail.com>
2020-06-24 04:16:24 +0900

    speaker and partner 's list and _slug pages .

---
+++ pages/speakers/_year/_slug.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <p>slug</p>
+    <p>{{ p.slug }}</p>
+    <p>params</p>
+    <p>{{ p }}</p>
+    <p>speakers</p>
+    <p>{{ speaker }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData ({ $content, params }) {
+    const speaker = await $content('speakers', params.year, params.slug).fetch()
+    const p = params
+    return { p, speaker }
+  }
+}
+</script>

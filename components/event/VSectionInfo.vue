b135dea1ed0208f9e9b97e27f7cdfead84dc2741
澁谷悠大 <univer405@gmail.com>
2020-06-24 16:35:03 +0900

    add event page.

---
+++ components/event/VSectionInfo.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-parallax dark src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg">
+
+    <v-row align="center" justify="center">
+      <v-col class="text-center" cols="12">
+        <h1 class="display-1 font-weight-bold mb-4">{{ title }}</h1>
+      </v-col>
+    </v-row>
+
+    <v-row align="start" justify="center">
+      <v-col cols="6">
+        <p class="subheading">{{abstruct}}</p>
+      </v-col>
+      <v-col class="text-center" cols="6">
+        <h1 class="display-1 font-weight-bold mb-4">概要</h1>
+        <p>{{date}}</p>
+      </v-col>
+    </v-row>
+
+  </v-parallax>
+</template>
+
+<script>
+export default {
+  props: ['title', 'abstruct', 'date']
+}
+</script>
+
+<style scoped>
+</style>>
---
+++ components/event/VSectionTheme.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-parallax height=1000 dark src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg">
+
+    <v-row align="center" justify="center">
+      <v-col class="text-center" cols="12">
+        <h1 class="display-1 font-weight-bold mb-4">{{ title }}</h1>
+      </v-col>
+    </v-row>
+
+    <v-row align="center" justify="center">
+      <v-col class="text-center" cols="5">
+        <h2 class="display-1 font-weight-bold mb-4">{{theme}}</h2>
+      </v-col>
+      <v-col  cols="7">
+        <v-img :src=keyVisual aspect-ratio="1.7"></v-img>
+      </v-col>
+    </v-row>
+
+    <v-row align="center" justify="center">
+      <v-col cols="12">
+        <h1 class="display-1 font-weight-bold mb-4">{{ wishs }}</h1>
+      </v-col>
+    </v-row>
+
+    <v-row align="center" justify="center">
+      <v-col class="text-center" cols="6">
+        <v-img src="https://picsum.photos/510/300?random" aspect-ratio="1.7"></v-img>
+      </v-col>
+      <v-col cols="6">
+        <div>
+          <p class="subheading">{{content}}</p>
+          <p class="subheading">TEDxNagoyaU2020 代表 れんれん</p>
+        </div>
+      </v-col>
+    </v-row>
+
+  </v-parallax>
+</template>
+
+<script>
+export default {
+  props: ['title', 'keyVisual', 'theme', 'wishs', 'content']
+}
+</script>
+
+<style scoped>
+</style>>

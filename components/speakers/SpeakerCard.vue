e7bb2a6ab939aa3eac6a60860679c7ce61bf8131
flat35hd99 <flat35hd99@gmail.com>
2020-06-24 22:04:09 +0900

    wip

---
+++ components/speakers/SpeakerCard.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-card>
+    <v-img :src="speaker.img"></v-img>
+    <v-card-title v-text="speaker.name" />
+    <v-card-subtitle v-text="speaker.position" />
+    <v-card-text v-text="speaker.leadSentence" />
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'SpeakerCard',
+  props: {
+    speaker: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
--- pages/speakers/_year/index.vue
+++ pages/speakers/_year/index.vue
@@ -1,20 +1,28 @@
 <template>
-  <div>
-    <p>slug</p>
-    <p>{{ p.slug }}</p>
-    <p>params</p>
-    <p>{{ p }}</p>
-    <p>speakers</p>
-    <p>{{ speakers }}</p>
-  </div>
+  <v-row>
+    <v-col
+      v-for="speaker in speakers"
+      :key="speaker.name"
+      cols="1"
+      sm="2"
+      md="3"
+      lg="4"
+    >
+      <speaker-card :speaker="speaker" />
+    </v-col>
+  </v-row>
 </template>
 
 <script>
+import SpeakerCard from '~/components/speakers/SpeakerCard.vue'
+
 export default {
+  components: {
+    SpeakerCard
+  },
   async asyncData ({ $content, params }) {
     const speakers = await $content('speakers', params.year).fetch()
-    const p = params
-    return { p, speakers }
+    return { params, speakers }
   }
 }
 </script>

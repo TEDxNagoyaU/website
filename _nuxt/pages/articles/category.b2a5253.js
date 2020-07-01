df0e58c46e98ea43b32529ca69b860946b83dbdf
flat35hd99 <flat35hd99@users.noreply.github.com>
2020-07-01 15:09:00 +0000

    deploy: dc899ea318fb7981b833b11af704df2aaf49026d

---
+++ _nuxt/pages/articles/category.b2a5253.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{331:function(t,e,n){"use strict";n.r(e);var l={head:function(){return{titleTemplate:"%s | category | TEDxNagoyaU"}}},c=n(32),component=Object(c.a)(l,(function(){var t=this.$createElement;return(this._self._c||t)("nuxt-child")}),[],!1,null,null,null);e.default=component.exports}}]);

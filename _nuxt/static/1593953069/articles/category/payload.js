adcd7e0048b6352c498c2804d590e665d7f680f2
flat35hd99 <flat35hd99@users.noreply.github.com>
2020-07-05 12:45:06 +0000

    deploy: d6b7cf86bb68977f6c387f725dd7f49cf92f5eed

--- _nuxt/static/1593953069/articles/category/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/articles/category", (function(a,b){return {data:[{},{},{allCategory:[{category:a},{category:b},{category:a},{category:b}]}],fetch:[],mutations:void 0}}("hogehoge","fugafuga")));

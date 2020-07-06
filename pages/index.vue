df0af31e94f7ce9ee6d86d8128b6f7f836e67b2e
flat35hd99 <flat35hd99@gmail.com>
2020-07-06 15:51:34 +0900

    add home

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,15 @@
 <template>
   <div>
-    <p>Hello world! this site is in development. You can contribute this rebuilding homepage project. </p>
-    <p>what? you say "I really realy want to contribute this project", right?</p>
-    <p>Thank you for your attention.</p>
-    <p>PLZ PLZ DONT HESITATE to ask any question. Don't hesitate to comment.</p>
-    <p>Ask us on Slack and Comment us on Google SpreadSheet.</p>
-    <nuxt-link to="/partners/2019">this link is partners/2019</nuxt-link>
+    <the-show-youtube />
   </div>
 </template>
 
 <script>
-
+import TheShowYoutube from '~/components/home/TheShowYoutube'
 export default {
+  components: {
+    TheShowYoutube
+  },
   head () {
     return {
       titleTemplate: 'TEDxNagoyaU'

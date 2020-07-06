b0f0ff1723e92c869a19cb8c80e20e7aa29777a4
flat35hd99 <flat35hd99@users.noreply.github.com>
2020-07-06 04:44:26 +0000

    deploy: 844b63405fbf0f6c5985436e75029513826a8e48

--- _nuxt/pages/speakers/index.e2f524e.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[20],{372:function(e,n,t){"use strict";t.r(n);var l=t(21),component=Object(l.a)({},(function(){var e=this.$createElement;return(this._self._c||e)("div",[this._v("this is speaker index")])}),[],!1,null,null,null);n.default=component.exports}}]);
--- _nuxt/static/1593958646/articles/tags/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/articles/tags", {data:[{},{},{}],fetch:[],mutations:void 0});

e367a8018a94d33ea1a2834897852577188a7039
澁谷悠大 <univer405@gmail.com>
2020-07-08 01:06:39 +0900

    change FirstView

--- components/home/FirstView.vue
+++ components/home/FirstView.vue
@@ -9,10 +9,14 @@
       justify="center"
     >
       <v-col class="text-center" cols="12">
-        <h1 class="display-1 font-weight-thin mb-4">2020 TedxNagoyaU Theme</h1>
+        <h1 class="display-1 font-weight-bold mb-4">TedxNagoyaU2020オンラインイベント開催が決定しました！</h1>
       </v-col>
       <v-col class="text-center" sm="6" xs="12">
+        <h1 class="display-1 font-weight-thin mb-4">2020 TedxNagoyaU Theme</h1>
         <v-img src="/keyVisual.png"></v-img>
+      </v-col>
+      <v-col class="text-center" sm="6" xs="12">
+        <h1 class="display-1 font-weight-thin mb-4">なんかださいどうしよう</h1>
         <v-btn class="ma-2" outlined color="white" href='/event'>View more</v-btn>
       </v-col>
     </v-row>

84f85d00a42b9afb5c43eb38fe3e94fea89bc5ff
flat35hd99 <flat35hd99@gmail.com>
2020-09-09 17:03:50 +0900

    /eventのfaqlistをコンポーネントにした。

--- nuxt/components/event/VSectionFaq.vue
+++ nuxt/components/event/VSectionFaq.vue
@@ -4,98 +4,64 @@
       <span>FAQ</span>
     </h1>
     <v-card
-    class="mx-auto"
-  >
-   <!--イベント開催前までのFAQ-->
-    <v-list>
-      <v-subheader>イベント開催前</v-subheader>
-      <v-list-group
-        value="true"
-        v-for="(faq, index) in faqs_before_event"
-        :key="index"
-      >
-        <template v-slot:activator>
-          <v-list-item-title>{{faq.question}}</v-list-item-title>
-        </template>
-        <v-divider></v-divider>
-        <v-list-item
-          v-for="answer in faq.answers"
-          :key="answer"
+      class="mx-auto"
+    >
+      <v-section-faq-list
+        listHeader="イベント前"
+        :faqs="faqs_before_event"
+      />
+    </v-card>
 
-        >
-            <v-list-item-content>
-              <v-list-item-subtitle v-html="answer"></v-list-item-subtitle>
-            </v-list-item-content>
-        </v-list-item>
-
-      </v-list-group>
-    </v-list>
-    <!--イベント本番中のFAQ-->
-     <v-list>
-      <v-subheader>イベント本番中</v-subheader>
-      <v-list-group
-        value="true"
-        v-for="(faq, index) in faqs_now_event"
-        :key="index"
-      >
-        <template v-slot:activator>
-          <v-list-item-title>{{faq.question}}</v-list-item-title>
-        </template>
-        <v-divider></v-divider>
-        <v-list-item
-          v-for="answer in faq.answers"
-          :key="answer"
-        >
-            <v-list-item-content>
-              <v-list-item-subtitle v-html="answer"></v-list-item-subtitle>
-            </v-list-item-content>
-        </v-list-item>
-
-      </v-list-group>
-    </v-list>
-
-  </v-card>
+    <v-card
+      class="mx-auto"
+    >
+      <v-section-faq-list
+      listHeader="イベント直前・イベント中"
+        :faqs="faqs_now_event"
+      />
+    </v-card>
   </div>
 </template>
 
 <script>
-  export default {
-    data: () => ({
-      admins: [
-        ['Management', 'people_outline'],
-        ['Settings', 'settings'],
-      ],
-      faqs_before_event: {
-        'faq_1' : {
-          'question' : '申し込みしたのに、メールが届かない' ,
-          'answers'  : ['<a href="mailto:info@tedxnagoyau.com">info@tedxnagoyau.com</a>に件名を申し込みメール不在にして送ってください。']},
-        'faq_2' : {
-          'question' : 'オープンチャットに入れない' ,
-          'answers'  : ['<a href="/event/open_chat">こちらのサイト</a>より、案内しております。', 'それでも入れない場合は、メールにて個別に対応させていただきます。']},
-        'faq_3' : {
-          'question' : '参加するブレイクのブースが分からない。' ,
-          'answers'  : ['ブースの詳細は下記に記載されています。また、希望されているブースはメールにてお問い合わせいただけますと、<br>現在のブースを通知いたします。']},
-        'faq_4' : {
-          'question' : '参加するブレイクのブースを変更したい' ,
-          'answers'  : ['メールにて希望のブース変更を受け付けております。ただし、定員のため対応できない可能性がございます。<br>また、オープンチャットでは対応しかねますことをご了承ください。', '期限は10月9日23：59です。']},
-        'faq_5' : {
-          'question' : 'イベント参加を取りやめたい' ,
-          'answers'  : ['メールまたはオープンチャットにて、お名前をご記名のもと、ご連絡ください。']},
+import VSectionFaqList from '~/components/event/VSectionFaqList.vue'
 
-      },
-      faqs_now_event: {
-        'faq_1' : {
-          'question' : '参加するブレイクのブースが違う' ,
-          'answers'  : ['メールまたはオープンチャットにて、お名前をご記名のもと、ご連絡ください。早急に対処させていただきます。オープンチャットでは、より素早く対応できますので、なるべくそちらの方で連絡してください。']},
-        'faq_2' : {
-          'question' : 'youtube視聴が出来ない' ,
-          'answers'  : ['メールまたはオープンチャットにて、お名前をご記名のもと、ご連絡ください。早急に対処させていただきます。オープンチャットでは、より素早く対応できますので、なるべくそちらの方で連絡してください。']},
-        'faq_3' : {
-          'question' : 'Zoomに参加できない' ,
-          'answers'  : ['メールまたはオープンチャットにて、お名前をご記名のもと、ご連絡ください。早急に対処させていただきます。オープンチャットでは、より素早く対応できますので、なるべくそちらの方で連絡してください。']},
-      }
-    }),
-  }
+export default {
+  components: {
+    VSectionFaqList
+  },
+  data: () => ({
+    faqs_before_event: {
+      'faq_1' : {
+        'question' : '申し込みしたのに、メールが届かない' ,
+        'answers'  : ['<a href="mailto:info@tedxnagoyau.com">info@tedxnagoyau.com</a>に件名を申し込みメール不在にして送ってください。']},
+      'faq_2' : {
+        'question' : 'オープンチャットに入れない' ,
+        'answers'  : ['<a href="/event/open_chat">こちらのサイト</a>より、案内しております。', 'それでも入れない場合は、メールにて個別に対応させていただきます。']},
+      'faq_3' : {
+        'question' : '参加するブレイクのブースが分からない。' ,
+        'answers'  : ['ブースの詳細は下記に記載されています。また、希望されているブースはメールにてお問い合わせいただけますと、<br>現在のブースを通知いたします。']},
+      'faq_4' : {
+        'question' : '参加するブレイクのブースを変更したい' ,
+        'answers'  : ['メールにて希望のブース変更を受け付けております。ただし、定員のため対応できない可能性がございます。<br>また、オープンチャットでは対応しかねますことをご了承ください。', '期限は10月9日23：59です。']},
+      'faq_5' : {
+        'question' : 'イベント参加を取りやめたい' ,
+        'answers'  : ['メールまたはオープンチャットにて、お名前をご記名のもと、ご連絡ください。']},
+
+    },
+    faqs_now_event: {
+      'faq_1' : {
+        'question' : '参加するブレイクのブースが違う' ,
+        'answers'  : ['メールまたはオープンチャットにて、お名前をご記名のもと、ご連絡ください。早急に対処させていただきます。オープンチャットでは、より素早く対応できますので、なるべくそちらの方で連絡してください。']},
+      'faq_2' : {
+        'question' : 'youtube視聴が出来ない' ,
+        'answers'  : ['メールまたはオープンチャットにて、お名前をご記名のもと、ご連絡ください。早急に対処させていただきます。オープンチャットでは、より素早く対応できますので、なるべくそちらの方で連絡してください。']},
+      'faq_3' : {
+        'question' : 'Zoomに参加できない' ,
+        'answers'  : ['メールまたはオープンチャットにて、お名前をご記名のもと、ご連絡ください。早急に対処させていただきます。オープンチャットでは、より素早く対応できますので、なるべくそちらの方で連絡してください。']},
+    }
+  })
+}
 </script>
 
 <style>
---
+++ nuxt/components/event/VSectionFaqList.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-list>
+    <v-subheader>{{ listHeader }}</v-subheader>
+    <v-list-group
+      value="true"
+      v-for="(faq, index) in faqs"
+      :key="index"
+    >
+      <template v-slot:activator>
+        <v-list-item-title>{{faq.question}}</v-list-item-title>
+      </template>
+      <v-divider></v-divider>
+      <v-list-item
+        v-for="answer in faq.answers"
+        :key="answer"
+      >
+        <v-list-item-content>
+          <v-list-item-subtitle v-html="answer"></v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list-group>
+  </v-list>
+</template>
+
+<script>
+export default {
+  name: 'VSectionFaqList',
+  props: {
+    listHeader: {
+      type: String,
+      required: true
+    },
+    faqs: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>

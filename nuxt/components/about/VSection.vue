8f7690e7eaf4e6855e5af3d86ec03454b1daba85
flat35hd99 <flat35hd99@gmail.com>
2020-09-17 22:48:05 +0900

    change design of about

--- nuxt/components/about/VSection.vue
+++ nuxt/components/about/VSection.vue
@@ -3,29 +3,33 @@
     outlined
     class="ma-1 mb-1"
   >
-    <v-row
-      align="center"
-      justify="center"
-    >
-      <v-col class="text-center" cols="6" sm="12">
-        <h1 class="display-1 font-weight-bold mb-4">
-          {{ title }}
-        </h1>
-      </v-col>
-    </v-row>
-    <v-row
-      align="center"
-      justify="center"
-    >
-      <v-col class="text-center" md="6" sm="12">
-        <v-img :src="src" aspect-ratio="1.7" />
-      </v-col>
-      <v-col md="5" sm="12">
-        <p class="subheading">
-          {{ content }}
-        </p>
-      </v-col>
-    </v-row>
+    <v-container>
+      <v-row
+        align="center"
+        justify="center"
+      >
+        <v-col class="text-center" cols="12">
+          <h1 class="display-1 font-weight-bold mb-4">
+            {{ title }}
+          </h1>
+        </v-col>
+      </v-row>
+      <v-row
+        align="center"
+        justify="center"
+      >
+        <v-col xs="12"  sm="12" md="6">
+          <v-img
+            :src="src"
+          />
+        </v-col>
+        <v-col xs="12" sm="12" md="5">
+          <p class="subheading">
+            {{ content }}
+          </p>
+        </v-col>
+      </v-row>
+    </v-container>
   </v-card>
 </template>
 

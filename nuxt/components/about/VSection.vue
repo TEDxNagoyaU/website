3251b9dd7da15052ad945003d507fd41466a367c
flat35hd99 <flat35hd99@gmail.com>
2020-09-29 00:39:08 +0900

    change about

--- nuxt/components/about/VSection.vue
+++ nuxt/components/about/VSection.vue
@@ -21,12 +21,11 @@
         <v-col xs="12" sm="12" md="6">
           <v-img
             :src="src"
+            :alt="alt"
           />
         </v-col>
         <v-col xs="12" sm="12" md="5">
-          <p class="subheading">
-            {{ content }}
-          </p>
+          <slot></slot>
         </v-col>
       </v-row>
     </v-container>
@@ -34,17 +33,20 @@
 </template>
 
 <script>
+/*
+ * slotには文章を入れる。
+ */
 export default {
   props: {
     title: {
       type: String,
       required: true
     },
-    content: {
+    src: {
       type: String,
       required: true
     },
-    src: {
+    alt: {
       type: String,
       required: true
     }

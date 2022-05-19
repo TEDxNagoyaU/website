685db356c2a84a3dcf6fcc5fa687718ef133ec9a
ozekikohei <88955369+ozekikohei@users.noreply.github.com>
2022-05-19 22:07:13 +0900

    Update FirstView.vue

--- nuxt/components/home/FirstView.vue
+++ nuxt/components/home/FirstView.vue
@@ -19,18 +19,18 @@
           <h1 class="display-1 font-weight-bold mb-4">
             TEDxNagoyaU2020オンラインイベント開催が決定しました！
           </h1>
-        </v-col>
+        </v-col> -->
                 
        <v-col class="text-center" sm="6" xs="12"> -
-          <h1 class="display-1 mb-4">新メンバー募集中！</h1>
-          <h2 class=" font-weight mb-4">集え大学生！！</h2>
+          <h1 class="display-1 mb-4">サロン開催決定！</h1>
+          <h2 class=" font-weight mb-4"></h2>
           <!-- @click.nativeすることで、nuxt-linkを使いながらもclickイベントでメソッドを起こすことができる。 -->
-          <!--
+          
           <v-btn
             class="ma-2"
             color="white"
             depressed
-            href="https://tedxnagoyau.github.io/2022SpringRecruit/"
+            href="https://tedxnagoyau.github.io/2022salon2_website/"
             target="_blank"
           >
             詳細 
@@ -43,7 +43,7 @@
             target="_blank"
           >
             参加申し込み
-          </v-btn> -->
+          </v-btn> 
         </v-col>
       </v-row>
     </v-container>

1ed792c247a64c1c55eec3183dce9d02144b132d
ozekikohei <88955369+ozekikohei@users.noreply.github.com>
2022-05-21 02:17:55 +0900

    Revert "Update FirstView.vue"
    This reverts commit ac7d15987b971cdcf9833b27d1551ee503850aa6.

--- nuxt/components/home/FirstView.vue
+++ nuxt/components/home/FirstView.vue
@@ -23,6 +23,7 @@
                 
        <v-col class="text-center" sm="6" xs="12"> -
           <h1 class="display-1 mb-4">サロン開催決定！</h1>
+          <h2 class=" font-weight mb-4"></h2>
           
           <v-btn
             class="ma-2"
